7fc3b5990baa55b6569ce6ed1df6f047cedabd3e
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2026-01-17 23:12:20 +0000

    updated files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-
 import Footer from "./components/Footer";
 
 function App() {
@@ -20,3 +19,4 @@ function App() {
 }
 
 export default App;
+
