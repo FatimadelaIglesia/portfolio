689560c9e96d65e0b2050f563ff5c306db3b9979
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2026-01-17 15:19:18 +0000

    update Contact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contacts";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
